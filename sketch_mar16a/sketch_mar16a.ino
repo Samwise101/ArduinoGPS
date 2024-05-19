7720f5068c82ce85e910b97f1a9e43eb301887a8
Samwise101 <samuel.palenik20@gmail.com>
2024-05-19 07:56:57 +0200

    added changes to data availablity

--- sketch_mar16a/sketch_mar16a.ino
+++ sketch_mar16a/sketch_mar16a.ino
@@ -9,11 +9,11 @@ double getNewestLng();
 void calculateGPSDistance();
 double haversine(double lat1, double lon1, double lat2, double lon2);
 
-static const uint8_t RXPin = 4;
-static const uint8_t TXpin = 3;
+static const uint8_t GPS_RXpin = 4;
+static const uint8_t GPS_TXpin = 3;
 static const long GPSbaud = 9600;
 
-uint8_t dataAvailable = 0;
+uint8_t gps_dataAvailable = 0;
 
 double gps_point_lat_lon_array[4][2];
 uint32_t gps_satelite_number = 0;
@@ -22,33 +22,30 @@ double gps_heading_degrees = 0.0;
 double fullDistanceTraveled = 0;
 
 TinyGPSPlus gps;
-SoftwareSerial ss(RXPin, TXpin);
+SoftwareSerial ss(GPS_RXpin, GPS_TXpin);
 
-unsigned long previousMillis = 0UL;
-unsigned long interval = 1000UL;
+unsigned long gps_previousMillis = 0UL;
+unsigned long gps_interval = 1000UL;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(GPSbaud);
   ss.begin(GPSbaud);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   gpsLoop();
-  }
+}
 
 void gpsLoop(){
-
   if (ss.available() > 0) {
     if(gps.encode(ss.read())){
       if(gps.location.isValid()){
         unsigned long currentMillis = millis();
 
-        if(currentMillis - previousMillis > interval){
+        if(currentMillis - gps_previousMillis > gps_interval){
           if(gps_point_counter < 4){
             getGPSPoint();
-            if(dataAvailable == 1){
+            if(gps_dataAvailable == 1){
               calculateGPSDistanceAndHeading();
             }
           }
@@ -62,89 +59,55 @@ void gpsLoop(){
           }
         }
       }
-      if(dataAvailable == 1){
-        double temp = -1;
-        temp = getNewestLat();
-        Serial.println();
-        Serial.print("Newest lat = ");
-        Serial.println(temp,10);
-
-        temp = getNewestLng();
-
-        Serial.print("Newest lng = ");
-        Serial.println(temp,10);
-
-        Serial.print("Number of satelites = ");
-        Serial.println(gps_satelite_number);
-      }
     }
+  }
+  else{
+    gps_dataAvailable = 0;
+  }
+
+  if(gps_dataAvailable){
+    Serial.print("New lat=  ");
+    Serial.println(getNewestLat(),10);
+    Serial.print("New lng=  ");
+    Serial.println(getNewestLng(),10);
+    gps_dataAvailable = 0;
   }
 }
 
 double getNewestLat(){
   if(gps_point_counter < 1 || gps_point_counter > 4){
-    return -1;
+    return -1.0;
   }
   return gps_point_lat_lon_array[gps_point_counter][0];
 }
 
 double getNewestLng(){
   if(gps_point_counter < 1 || gps_point_counter > 4){
-    return -1;
+    return -1.0;
   }
-
   return gps_point_lat_lon_array[gps_point_counter][1];
 }
 
 double haversine(double lat1, double lon1, double lat2, double lon2) {
     const double rEarth2 = 6372795.0;
-    const double rEarth = 6371000.0; // in meters
+    const double rEarth = 6371000.0;
     double x = pow( sin( ((lat2 - lat1)*M_PI/180.0) / 2.0), 2.0 );
     double y = cos(lat1*M_PI/180.0) * cos(lat2*M_PI/180.0);
     double z = pow( sin( ((lon2 - lon1)*M_PI/180.0) / 2.0), 2.0 );
     double a = x + y * z;
     double c = 2.0 * atan2(sqrt(a), sqrt(1.0-a));
     double d = rEarth2 * c;
-    return d; // in meters
-}
-
-void calculateGPSDistanceAndHeading(){
-  double distance = haversine(gps_point_lat_lon_array[gps_point_counter-2][0] , gps_point_lat_lon_array[gps_point_counter-2][1], 
-                             gps_point_lat_lon_array[gps_point_counter-1][0], gps_point_lat_lon_array[gps_point_counter-1][1]);
-
-  double distance2 = gps.distanceBetween(gps_point_lat_lon_array[gps_point_counter-2][0] , gps_point_lat_lon_array[gps_point_counter-2][1], 
-                             gps_point_lat_lon_array[gps_point_counter-1][0], gps_point_lat_lon_array[gps_point_counter-1][1]);
-
-  if(distance2 > 2.0 || distance > 2.0){
-    distance = 0.0;
-    distance2 = 0.0;
-  }
-
-  distance = (distance + distance2)/2.0;
-
-  if(distance != 0.0){
-    fullDistanceTraveled += distance;
-  }
-
-  gps_heading_degrees = gps.courseTo(gps_point_lat_lon_array[gps_point_counter-2][0] , gps_point_lat_lon_array[gps_point_counter-2][1], 
-                              gps_point_lat_lon_array[gps_point_counter-1][0], gps_point_lat_lon_array[gps_point_counter-1][1]);
-
-  Serial.print(distance,10);
-  Serial.print(",");
-  Serial.print(fullDistanceTraveled,10);
-  Serial.print(",");
-  Serial.print(gps_heading_degrees,10);
+    return d;
 }
 
 void getGPSPoint(){
   if(gps_point_counter < 4){
     if (gps.location.isValid() && gps.location.isUpdated()) {
-
       if(gps_point_counter > 1)
-        dataAvailable = 1;
+        gps_dataAvailable = 1;
       else
-        dataAvailable = 0;
-    //  displayInfo();
+        gps_dataAvailable = 0;
+
       Serial.print("1");
       Serial.print(",");
       
@@ -163,7 +126,27 @@ void getGPSPoint(){
       gps_point_counter++;
     }
     else{
-      dataAvailable = 0;
+      gps_dataAvailable = 0;
     }
   }
+}
+
+void calculateGPSDistanceAndHeading(){
+  double distance = haversine(gps_point_lat_lon_array[gps_point_counter-2][0] , gps_point_lat_lon_array[gps_point_counter-2][1], 
+                             gps_point_lat_lon_array[gps_point_counter-1][0], gps_point_lat_lon_array[gps_point_counter-1][1]);
+  if(distance > 2.0){
+    distance = 0.0;
+  }
+
+  if(distance != 0.0){
+    fullDistanceTraveled += distance;
+  }
+
+  gps_heading_degrees = gps.courseTo(gps_point_lat_lon_array[gps_point_counter-2][0] , gps_point_lat_lon_array[gps_point_counter-2][1], 
+                              gps_point_lat_lon_array[gps_point_counter-1][0], gps_point_lat_lon_array[gps_point_counter-1][1]);
+  Serial.print(distance,5);
+  Serial.print(",");
+  Serial.print(fullDistanceTraveled,5);
+  Serial.print(",");
+  Serial.println(gps_heading_degrees);
 }
